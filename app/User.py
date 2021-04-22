f3f031c3422e5569607f510f191fbf5cfa99d3cb
rit1217 <57401346+rit1217@users.noreply.github.com>
2021-04-22 17:51:07 +0700

    Create User class and UserModel class.

---
+++ app/User.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__( self, username, password, firstName, lastName, height, weight, birthDate, gender ):
+        self.username = username
+        self.password = password
+        self.firstName = firstName
+        self.lastName = lastName
+        self.height = height
+        self.weight = weight
+        self.birthDate = birthDate
+        self.gender = gender
+
+        
--- app/databasemodel/test.py
+++ app/databasemodel/test.py
@@ -1,8 +1,32 @@
 import sqlite3
+from passlib.hash import pbkdf2_sha256
 
-conn = sqlite3.connect("app/databasemodel/main_data.db")
-c = conn.cursor()
-c.execute( "SELECT USERNAME FROM Recipe")
-print( "\n", c.fetchall() )
-conn.commit()
-conn.close()
+
+class UserModel:
+    def __init__(self):
+        try:
+            conn = sqlite3.connect("app/databasemodel/main_data.db")
+        except:
+            print ("Cannot connect to the database.")
+        self.cursor = conn.cursor()
+        self.cursor.execute( "SELECT USERNAME FROM Recipe")
+        print( "\n", c.fetchall() )
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def insertUser( self, newUser ):
+        username = newUser.getUsername()
+        password = pbkdf2_sha256.hash( newUser.getPassword())
+        firstName = newUser.getFirstName()
+        lastName = newUser.getLastName()
+        height = newUser.getHeight()
+        weight = newUser.getWeight()
+        birthDate = newUser.getBirthDate()
+        gender = newUser.getGender()
+        self.cursor.execute( "INSERT INTO User(USERNAME, PASSWORD, FIRST_NAME, LAST_NAME, HEIGHT, WEIGHT, BIRTHDATE, GENDER) VALUE(?,?,?,?,?,?,?,?)",
+        (username, password, firstName, lastName, height, weight, birthDate, gender))
+
+if __name__ == "__main__":
+    temp = pbkdf2_sha256.hash( "eiei")
+    print( pbkdf2_sha256.verify( "eiei", temp ) )
